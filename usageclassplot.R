ebaee74e28dd3013aecfa773be6f03c5e7830109
AdamSkog <adamskoglund2022@gmail.com>
2023-03-07 11:51:50 -0800

    Usageclassplot trendline added

--- usageclassplot.R
+++ usageclassplot.R
@@ -6,8 +6,10 @@ cbt <- read_delim("Checkouts_by_Title.csv")
 
 # Usage class changing over time
 cbt %>% 
+  filter(CheckoutYear != 2023) %>% 
   mutate(time = CheckoutYear + CheckoutMonth/12) %>% 
-  group_by(time, UsageClass) %>% 
+  group_by(CheckoutYear, UsageClass) %>% 
   summarize(checkoutmean = sum(Checkouts)) %>% 
-  ggplot() + geom_line(aes(time, checkoutmean, col = UsageClass)) +
+  ggplot(aes(CheckoutYear, checkoutmean, col = UsageClass)) + geom_point() + geom_line() +
+  geom_smooth(method = lm, se = F) +
   labs(x = "Time", y = "Number of Checkouts", col = "Type")
