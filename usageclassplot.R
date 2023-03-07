23452d55ce67e661d21c7475d0cbc2e31ffceab9
AdamSkog <adamskoglund2022@gmail.com>
2023-03-06 17:39:11 -0800

    Usageclass Plot Added

---
+++ usageclassplot.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+# Load dataset
+cbt <- read_delim("Checkouts_by_Title.csv")
+
+# Usage class changing over time
+cbt %>% 
+  mutate(time = CheckoutYear + CheckoutMonth/12) %>% 
+  group_by(time, UsageClass) %>% 
+  summarize(checkoutmean = mean(Checkouts)) %>% 
+  ggplot() + geom_line(aes(time, checkoutmean, col = UsageClass)) +
+  labs(x = "Time", y = "Number of Checkouts", col = "Type")
